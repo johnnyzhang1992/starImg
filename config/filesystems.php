587b0d34b2e98e60669e4621d9c77d33de24b6df
johnnyzhang <1113638674@qq.com>
2018-06-04 21:42:34 +0800

    update

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,7 +46,7 @@ return [
         'local' => [
             'driver' => 'local',
 //            'root' => storage_path('app'),
-            'root' => storage_path('/'),
+            'root' => public_path('/'),
         ],
 
         'public' => [
